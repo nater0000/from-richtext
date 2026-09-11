3b45697ac53296e0f01b238e1b529c24319e3f2f
Nathan <123835325+nater0000@users.noreply.github.com>
2026-09-10 18:38:24 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  // Replace this with your exact GitHub repository name
-  base: '/from-richtext/', 
+  base: '/from-richtext/',
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Rich Text to MD',
+        short_name: 'RT2MD',
+        theme_color: '#1e1e1e',
+        background_color: '#000000',
+        display: 'standalone',
+        icons: [
+          // Place two placeholder images named icon-192.png and icon-512.png in your /public folder
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
 })
